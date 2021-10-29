6b43b3fea83701e13b4be22c60b2157713e77d00
高晓东 <gaoxiaodong04@meituan.com>
2021-10-29 17:51:33 +0800

    add version utils

---
+++ src/main/java/com/uraurora/dependency/resolver/util/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.uraurora.dependency.resolver.util;
+
+/**
+ * @author : gaoxiaodong04
+ * @program : dependency-resolve-sdk
+ * @date : 2021-10-28 15:03
+ * @description :
+ */
+public class CommonUtils {
+}
